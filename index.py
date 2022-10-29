b3ef0ac9b67b6bd2f5282a418e6214fa5f1207db
Prateek T L <tlprateektl@gmail.com>
2022-10-29 21:56:20 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, Prateek!</p>"
+  
+if __name__ == "__main__" :
+  helloworld.run(host="0.0.0.0", port=int("3000"), debug=True)
