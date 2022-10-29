d23fa154d98230ff6db8a5f380f923bf225b7ec5
Prateek T L <tlprateektl@gmail.com>
2022-10-29 22:23:04 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,8 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, Prateek!</p>"
+helloworld = Flask(__name__)
+@helloworld.route("/")
+def run():
+    return "{\"message\":\"Hello, Prateek!\"}"
   
 if __name__ == "__main__" :
   helloworld.run(host="0.0.0.0", port=int("3000"), debug=True)
